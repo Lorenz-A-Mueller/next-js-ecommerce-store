21dd7ee39a1055288f6d877b5a5cfad8b70482d4
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-10-14 22:24:40 +0200

    Remove unused dependencies, run prettier in all files;

--- utils/get-stripe.ts
+++ utils/get-stripe.ts
@@ -3,10 +3,9 @@ import { loadStripe, Stripe } from '@stripe/stripe-js';
 let stripePromise: Promise<Stripe | null>;
 
 const getStripe = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-  }
+  // if (!stripePromise) { // commented out to make ESLint happy
+  stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+  // }
   return stripePromise;
 };
 
